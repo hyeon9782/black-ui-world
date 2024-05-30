8a548fcd6dba942d830df8beb9b454b55ff1fbd0
jeongho <hyeon9782@gmail.com>
2024-05-30 22:27:02 +0900

    feat: ArticlePreview 스타일링 ArticlePage 데이터 패칭

--- src/components/article/ArticlePreview.tsx
+++ src/components/article/ArticlePreview.tsx
@@ -1,22 +1,34 @@
-import { Card, Tag } from "@black-ui/react";
+import { Button, Card, Tag } from "@black-ui/react";
 import { Article } from "../../types/articles";
 import UserBox from "../user/UserBox";
+import { useNavigate } from "@tanstack/react-router";
 type Props = {
   article: Article;
 };
 const ArticlePreview = ({ article }: Props) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate({ to: `/article/${article.slug}` });
+  };
   return (
-    <Card style={{ boxSizing: "border-box" }}>
-      <div>
+    <Card style={{ boxSizing: "border-box" }} onClick={handleClick}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
         <UserBox profile={article.author} createdAt={article.createdAt} />
         <div>
-          <button></button>
+          <Button>{article.favoritesCount}</Button>
         </div>
       </div>
       <div
         style={{
           fontWeight: "bold",
           fontSize: "24px",
+          height: "30px",
         }}
       >
         {article.title}
@@ -25,16 +37,26 @@ const ArticlePreview = ({ article }: Props) => {
         style={{
           overflow: "hidden",
           textOverflow: "ellipsis",
-          width: "300px",
-          height: "200px",
+          width: "100%",
+          height: "50px",
         }}
       >
-        {article.body}
+        {article.description}
       </div>
-      <div style={{ display: "flex", gap: 10 }}>
-        {article.tagList.map((tag) => (
-          <Tag>{tag}</Tag>
-        ))}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <span>Read more...</span>
+        <div style={{ display: "flex", gap: 10 }}>
+          {article.tagList.map((tag) => (
+            <Tag>{tag}</Tag>
+          ))}
+        </div>
       </div>
     </Card>
   );
--- src/routes/article.$slug.tsx
+++ src/routes/article.$slug.tsx
@@ -1,25 +1,46 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useParams } from "@tanstack/react-router";
 import UserBox from "../components/user/UserBox";
 import { Button } from "@black-ui/react";
+import { getArticle } from "../apis/articles";
+import { useEffect, useState } from "react";
+import { Article } from "../types/articles";
 
 export const Route = createFileRoute("/article/$slug")({
-  component: () => ArticlePage,
+  component: ArticlePage,
 });
 
 export function ArticlePage() {
+  const { slug } = useParams({ from: "/article/$slug" });
+  const [article, setArticle] = useState<Article>();
+
+  const fetchArticle = async () => {
+    const response = await getArticle(slug);
+    console.log(response);
+    setArticle({
+      ...response.data.article,
+    });
+  };
+
+  useEffect(() => {
+    fetchArticle();
+  }, []);
   return (
     <>
-      <div>
-        <h1></h1>
-        <div>
-          <UserBox />
+      {article?.slug && (
+        <>
           <div>
-            <Button>Follow Maksim Estenban</Button>
-            <Button>Favorite Article</Button>
+            <h1>{article.title}</h1>
+            <div>
+              <UserBox profile={article.author} createdAt={article.createdAt} />
+              <div>
+                <Button>Follow Maksim Estenban</Button>
+                <Button>Favorite Article</Button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div></div>
+          <div>{article.body}</div>
+        </>
+      )}
     </>
   );
 }
